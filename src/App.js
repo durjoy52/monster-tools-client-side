428165b44ff632cae4720c1f027a5d51f6e29bcc
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-05-29 05:06:44 +0600

    purchase page set

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import SignUp from './components/Login/SignUp';
+import Purchase from './components/Purchase/Purchase';
 import RequireAuth from './components/REquireAuth';
 import Footer from './components/Shared/Footer';
 import Navbar from './components/Shared/Navbar/Navbar';
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+          <Route path='/purchase' element={<RequireAuth><Purchase/></RequireAuth>}></Route>
           <Route path='/dashboard' element={<RequireAuth><Dashboard></Dashboard></RequireAuth>}>
             <Route path='addproduct' element={<AddProducts/>}></Route>
           </Route>
--- src/components/Purchase/Purchase.js
+++ src/components/Purchase/Purchase.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../Loading/Loading';
 
 const Purchase = () => {
+    const [user,loading] = useAuthState(auth)
+    if(loading){
+        return<Loading/>
+    }
     return (
         <div>
-            
+            <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+      <div className="text-center">Purchase now !!</div>
+    <form>
+        <input className="input input-bordered w-full max-w-xs" type="text" value={user?.displayName} disabled/>
+        <input className="input input-bordered w-full max-w-xs" type="text" value={user?.email} disabled/>
+        <input className="input input-bordered w-full max-w-xs" type="text" name='phone' placeholder='phone number' />
+        <input type="number" name="order_quantity" className="input input-bordered w-full max-w-xs" />
+    </form>
+  </div>
+</div>
         </div>
     );
 };
